9a7d8f0a92caa32a09f8c73fa46c03afd9207afe
konfig-publisher <publisher@konfigthis.com>
2023-12-04 19:43:07 +0000

    Version Bump SDKs

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "humanloop"
-VERSION = "0.6.0-rc.9"
+VERSION = "0.6.0-rc.10"
 # To install the library, run the following
 #
 # python setup.py install
