aea39e649bd1b0ed3fbdecc6d4e09eac2c13d0ae
konfig-publisher <publisher@konfigthis.com>
2023-12-07 19:05:43 +0000

    Regenerate SDKs

--- humanloop/paths/evaluators/get.pyi
+++ humanloop/paths/evaluators/get.pyi
@@ -110,7 +110,7 @@ class BaseApi(api_client.Api):
         AsyncGeneratorResponse,
     ]:
         """
-        List 
+        List
         :param skip_deserialization: If true then api_response.response will be set but
             api_response.body and api_response.headers will not be deserialized into schema
             class instances
@@ -205,7 +205,7 @@ class BaseApi(api_client.Api):
         api_client.ApiResponseWithoutDeserialization,
     ]:
         """
-        List 
+        List
         :param skip_deserialization: If true then api_response.response will be set but
             api_response.body and api_response.headers will not be deserialized into schema
             class instances
