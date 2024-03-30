322e17c18fa8b1f9fcccb3d5b36e7902e34d7df9
konfig-publisher <publisher@konfigthis.com>
2024-03-30 10:11:35 +0000

    Version Bump SDKs

--- humanloop/configuration.py
+++ humanloop/configuration.py
@@ -423,7 +423,7 @@ conf = humanloop.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 4.0.1\n"\
-               "SDK Package Version: 0.7.16".\
+               "SDK Package Version: 0.7.17".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
