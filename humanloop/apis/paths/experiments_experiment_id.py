3d5233a855c12757964c956184f3d1b7c9ee2f5b
konfig-publisher <publisher@konfigthis.com>
2024-07-19 20:05:04 +0000

    Regenerate SDKs

--- humanloop/apis/paths/experiments_experiment_id.py
+++
@@ -1,9 +0,0 @@
-from humanloop.paths.experiments_experiment_id.delete import ApiFordelete
-from humanloop.paths.experiments_experiment_id.patch import ApiForpatch
-
-
-class ExperimentsExperimentId(
-    ApiFordelete,
-    ApiForpatch,
-):
-    pass
