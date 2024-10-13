8742e85a16668ce94dfdbe8eea948e87438db7d3
Peter Hayes <ucabpnh@ucl.ac.uk>
2024-10-13 16:07:58 -0400

    Extend evals client with run method

--- src/humanloop/client.py
+++ src/humanloop/client.py
@@ -1,11 +1,46 @@
 import typing
+from typing import Optional, List, Sequence
 import os
 import httpx
-from functools import partial
 
 from .base_client import BaseHumanloop, AsyncBaseHumanloop
 from .environment import HumanloopEnvironment
-from .eval_utils import _run_eval
+from .eval_utils import _run_eval, Dataset, File, Evaluator, EvaluatorCheck
+from .base_client import EvaluationsClient
+
+class ExtendedEvalsClient(EvaluationsClient):
+
+    client: BaseHumanloop
+
+    def run(
+        self,
+        file: File,
+        name: Optional[str],
+        dataset: Dataset,
+        evaluators: Optional[Sequence[Evaluator]] = None,
+        # logs: typing.Sequence[dict] | None = None,
+        workers: int = 4,
+    ) -> List[EvaluatorCheck]:
+        """
+        Evaluate your function for a given `Dataset` and set of `Evaluators`.
+        :param file: the Humanloop file being evaluated, including a function to run over the dataset.
+        :param name: the name of the Evaluation to run. If it does not exist, a new Evaluation will be created under your File.
+        :param dataset: the dataset to map your function over to produce the outputs required by the Evaluation.
+        :param evaluators: define how judgments are provided for this Evaluation.
+        :param workers: the number of threads to process datapoints using your function concurrently.
+        :return: per Evaluator checks.
+        """
+        if self.client is None:
+            raise ValueError("Need Humanloop client defined to run evals")
+
+        return _run_eval(
+            client=self.client,
+            file=file,
+            name=name,
+            dataset=dataset,
+            evaluators=evaluators,
+            workers=workers,
+        )
 
 
 class Humanloop(BaseHumanloop):
@@ -37,7 +72,9 @@ class Humanloop(BaseHumanloop):
             follow_redirects=follow_redirects,
             httpx_client=httpx_client,
         )
-        self.evaluations.run = partial(_run_eval, client=self)  # type: ignore[attr-defined]
+        eval_client = ExtendedEvalsClient(client_wrapper=self._client_wrapper)
+        eval_client.client = self
+        self.evaluations = eval_client
 
 
 class AsyncHumanloop(AsyncBaseHumanloop):
