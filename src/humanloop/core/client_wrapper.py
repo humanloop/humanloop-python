0fe1c911f5c2ba5579c9e0feba63621ef3a47e98
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-02-17 12:18:19 +0000

    Release 0.8.25b2

--- src/humanloop/core/client_wrapper.py
+++ src/humanloop/core/client_wrapper.py
@@ -16,7 +16,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "humanloop",
-            "X-Fern-SDK-Version": "0.8.25b1",
+            "X-Fern-SDK-Version": "0.8.25b2",
         }
         headers["X-API-KEY"] = self.api_key
         return headers
